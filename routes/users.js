916c170f3707c80b5a741dc1b7afe9af5971faa1
eastlife <dengbx1212@163.com>
2020-05-30 23:35:59 -0400

    Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -39,14 +41,22 @@ router.post('/', [
 
             await user.save();
 
-            res.send('User saved');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn: 3600
+            }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
         }
-
-        res.send('passed');
     });
 
 module.exports = router;
