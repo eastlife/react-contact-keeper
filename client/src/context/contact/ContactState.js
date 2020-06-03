efd23be10703b36f9cf7bfed1e00a75c9a5d51a7
eastlife <dengbx1212@163.com>
2020-06-03 19:01:13 -0400

    [important] Contact Filter Form & State, useRef

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,7 +38,8 @@ const ContactState = props => {
                 type: 'professional'
             }
         ],
-        current: null
+        current: null,
+        filtered: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -47,42 +48,51 @@ const ContactState = props => {
     const addContact = contact => {
         contact.id = uuidv4();
         dispatch({ type: ADD_CONTACT, payload: contact });
-    }
+    };
 
     // Delete Contact
     const deleteContact = id => {
         dispatch({ type: DELETE_CONTACT, payload: id });
-    }
+    };
 
     // Set Current Contact
     const setCurrent = contact => {
         dispatch({ type: SET_CURRENT, payload: contact });
-    }
+    };
 
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
-    }
+    };
 
     // Update Contact
     const updateContact = contact => {
         dispatch({ type: UPDATE_CONTACT, payload: contact });
-    }
+    };
 
     // Filter Contacts
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text});
+    };
 
     // Clear Filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER});
+    };
 
     return (
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
                 current: state.current,
+                filtered: state.filtered,
                 addContact,
                 deleteContact,
                 updateContact,
                 setCurrent,
-                clearCurrent
+                clearCurrent,
+                filterContacts,
+                clearFilter
             }}
         >
             {props.children}
