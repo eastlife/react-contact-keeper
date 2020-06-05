07af53789962c83216661885ac75ae2b77ca4a03
eastlife <dengbx1212@163.com>
2020-06-04 23:12:48 -0400

    update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -56,7 +56,7 @@ const ContactState = props => {
     };
 
     // Delete Contact
-    const deleteContact = async(id) => {
+    const deleteContact = async (id) => {
         try {
             await axios.delete(`/api/contacts/${id}`);
             dispatch({ type: DELETE_CONTACT, payload: id });
@@ -64,11 +64,28 @@ const ContactState = props => {
             dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
         }
     };
+
+    // Update Contact
+    const updateContact = async (contact) => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+            // res.data to be the updated contact
+            dispatch({ type: UPDATE_CONTACT, payload: res.data });
+        } catch (err) {
+            dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+        }
+    };
+
     // Clear Contacts
     const clearContacts = () => {
         dispatch({ type: CLEAR_CONTACTS });
     };
-
 
     // Set Current Contact
     const setCurrent = contact => {
@@ -78,11 +95,6 @@ const ContactState = props => {
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
-    };
-
-    // Update Contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact });
     };
 
     // Filter Contacts
